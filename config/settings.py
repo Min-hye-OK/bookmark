33b48420a6796ab7c8fd96a78b4ca9b4f0270547
Min-hye-OK <ingminham@naver.com>
2020-02-13 12:32:57 +0900

    Bookmark Service

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'w$s#an55@hzyvf5y19k=pa!u1pu@!e(krhu*hak=%&vem&wk^&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
